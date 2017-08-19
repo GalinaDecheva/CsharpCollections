150aff2e76fda5db17d2639b06bbadd32565f83a
Galina Decheva <de_galina@abv.bg>
2017-08-19 12:25:30 +0300

    Add project files.

---
+++ MinMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input1 = int.Parse(Console.ReadLine());
+            int input2 = int.Parse(Console.ReadLine());
+            int input3 = int.Parse(Console.ReadLine());
+           
+
+            Console.WriteLine(GetMin(input1, GetMin(input2, input3)));
+
+        }
+
+
+        public static int GetMin(int a, int b)
+        {
+            if (a < b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+    }
+}
