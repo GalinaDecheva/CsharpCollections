64d83e913c2d9aca2ff3d3e3546a4118c91b8a8e
Galina Decheva <de_galina@abv.bg>
2017-08-20 10:50:40 +0300

    C#

---
+++ CountofGivenElementinArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountofGivenElementinArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int input2 = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            for (int i = 0; i < input1.Length; i++)
+            {
+                if (input1[i] == input2)
+                {
+                    count += 1;
+
+                }
+            }
+            Console.WriteLine(count);
+        }
+        }
+}
---
+++ StringRepeater/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringRepeater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int input1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            string[] input1 = Console.ReadLine().Split(' ');
+            int input2 = int.Parse(Console.ReadLine());
+            StringRepeater(input1, input2);
+        }
+
+        static string StringRepeater(string str, int count)
+        {
+            string StringRepeater = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine();
+            }
+            return StringRepeater;
+        }
+
+        
+    }
+}
