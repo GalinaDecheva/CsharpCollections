4b93e88fa0bf58b5d9c8b3c5a0362cb524396b13
Galina Decheva <de_galina@abv.bg>
2017-08-21 22:39:01 +0300

    Nth

---
+++ NthDigit/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NthDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long number = long.Parse(Console.ReadLine());
+            int index = int.Parse(Console.ReadLine());
+            Console.WriteLine(FindNthDigit(number, index));
+
+        }
+
+        static int FindNthDigit(long number, int index)
+        {
+            while (number > 0 )
+            {
+                if (index == 1)
+                {
+                    return (int)(number % 10);
+                }
+                number /= 10;
+                index--;
+            }
+            return 0;
+        }
+
+    }
+
+
+}
+
+    
+
