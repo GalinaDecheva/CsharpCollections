49f580323ddbb50e7bd4bb1db36a2657823d510b
Galina Decheva <de_galina@abv.bg>
2017-08-22 22:17:26 +0300

    IntegerToString

---
+++ IntegerToBase/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerToBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long number = long.Parse(Console.ReadLine());
+            int toBase = int.Parse(Console.ReadLine());
+            Console.WriteLine(IntegerToBase(number, toBase));
+        }
+
+        static string IntegerToBase(long number, int toBase)
+        {
+            string result = string.Empty;
+            while (number > 0)
+            {
+                result += Convert.ToString(number % toBase);
+                number /= toBase;
+            }
+            return new string(result.ToCharArray().Reverse().ToArray());
+        }
+    }
+}
+
