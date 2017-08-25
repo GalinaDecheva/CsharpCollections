52b5f3adde870077cebae83442dd6b73c42a85bb
Galina Decheva <de_galina@abv.bg>
2017-08-26 00:18:29 +0300

    Notification

---
+++ Notification1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine();
+                string operation;
+                string message;
+                int code;
+                if (command == "success")
+                {
+                    operation = Console.ReadLine();
+                    message = Console.ReadLine();
+                    Console.WriteLine(ShowSuccess(operation, message));
+                }
+                else if (command == "error")
+                {
+                    operation = Console.ReadLine();
+                    code = int.Parse(Console.ReadLine());
+                    Console.WriteLine(ShowError(operation, code));
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
+        static String ShowSuccess(string operation, string message)
+        {
+            return ("Successfully executed" + operation + ".\n" + "==============================\n" +
+             "Message: " + message + ".");
+        }
+
+        static String ShowError(string operation, int code)
+        {
+            return ("Error: Failed to execute " + operation + ".\n" + "==============================\n" +
+             "Error Code: " + code + ".\n" + "Reason: " + Reason(code) + ".");
+
+        }
+
+        static string Reason(int code)
+        {
+            if (code > 0)
+            {
+                return ("Invalid Client Data");
+            }
+            else
+            {
+                return ("Internal System Failure");
+            }
+        }
+    }
+}
+
+
+
+
+
