c862fceaf7a7d4e2ad1661deda3f8fc951421212
Galina Decheva <de_galina@abv.bg>
2017-08-27 19:43:12 +0300

    Final commit

--- NumToWord/Program.cs
+++ NumToWord/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NumToWord
 {
@@ -10,6 +6,60 @@ namespace NumToWord
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Console.WriteLine(Letterize(input));
+            }
+        }
+
+        static string Letterize(int number)
+        {
+            bool isNegative = number < 0;
+            string result = string.Empty;
+
+            string[] words = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+            "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+
+            string[] wordsDecades = { "", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+            if (number > 999)
+            {
+                result = "too large";
+            }
+
+            if (number < -999)
+            {
+                result = "too small";
+            }
+
+            if (isNegative) number = -number;
+            bool isThreeDigit = (number > 99) && (number < 1000);
+
+            if (isThreeDigit)
+            {
+                if (isNegative)
+                {
+                    result += "minus ";
+                }
+                result += words[number / 100] + "-hundred";
+                number %= 100;
+                if(number != 0)
+                {
+                    result += " and ";
+                    if(number < 20)
+                    {
+                        result += words[number];
+                    }
+                    else
+                    {
+                        result += wordsDecades[number / 10] + " " + words[number % 10];
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
