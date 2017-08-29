3da8f235affa1588ced8f5cc1898329c93308740
Galina Decheva <de_galina@abv.bg>
2017-08-29 23:19:08 +0300

    StringEncryption

---
+++ StringEncryption/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace StringEncryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[] arr = new char[n];
+            string output = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = char.Parse(Console.ReadLine());
+            }
+            foreach(char letter in arr)
+            {
+                output += Encrypt(letter);
+            }
+            Console.WriteLine(output);
+        }
+
+        static string Encrypt(char letter)
+        {
+            string result = string.Empty;
+            result += GetFirstChar(letter);
+            result += ConvertIntToChar(GetFirstDigit(letter));
+            result += ConvertIntToChar(GetLastDigit(letter));
+            result += GetLastChar(letter);
+            return result;
+        }
+
+        static int GetFirstDigit(char letter)
+        {
+            if (letter > 99) return letter / 100;
+            return letter / 10;
+        }
+
+        static int GetLastDigit(char letter)
+        {
+            return letter % 10;
+        }
+
+        static char GetFirstChar(char letter)
+        {
+            return (char)(letter + GetLastDigit(letter));
+        }
+
+        static char GetLastChar(char letter)
+        {
+            return (char)(letter - GetFirstDigit(letter));
+        }
+
+        static char ConvertIntToChar(int input)
+        {
+            return (char)(input + 48);
+        }
+    }
+}
