073b35569e0e79a07a69a0027b3f461ff71057b0
Galina Decheva <de_galina@abv.bg>
2017-08-29 20:49:54 +0300

    CountOfNegativeElementsInArray

---
+++ CountOfNegativeElementsInArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountOfNegativeElementsInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            int temp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+                if (arr[i] < 0)
+                {
+                    temp += 1;
+                }
+            }
+            Console.WriteLine(temp);
+        }
+    }
+}
