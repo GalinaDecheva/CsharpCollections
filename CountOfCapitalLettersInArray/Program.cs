e08028693c83c232890be48bca7bfc8f8fd9ebe8
Galina Decheva <de_galina@abv.bg>
2017-08-31 23:46:01 +0300

    CountOfCapitalLettersInArray

---
+++ CountOfCapitalLettersInArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CountOfCapitalLettersInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(' ');
+            int count = 0;
+            foreach(string word in arr)
+            {
+                if (word.Length == 1 && word[0] >= 65 && word[0] <= 90) count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
