327cfc67cef202c0f17fe3337cbcf17d174ae5d0
Galina Decheva <de_galina@abv.bg>
2017-09-03 22:56:47 +0300

    Altitude

---
+++ Altitude/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altitude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(' ');
+            bool isCrashed = false;
+            int altitude = int.Parse(arr[0]);
+            for (int i = 1; i < arr.Length; i+=2)
+            {
+                if (altitude <= 0)
+                {
+                    isCrashed = true;
+                    break;
+                }
+                int change = int.Parse(arr[i + 1]);
+                if(arr[i] == "up")
+                {
+                    altitude += change;
+                } else
+                {
+                    altitude -= change;
+                }
+                if (altitude <= 0)
+                {
+                    isCrashed = true;
+                    break;
+                }
+
+
+            }
+            if(isCrashed)
+            {
+                Console.WriteLine("crashed");
+            } else
+            {
+                Console.WriteLine("got through safely. current altitude: {0}m", altitude);
+            }
+          
+        }
+    }
+}
