135f88866fc2c9520535f27f6e6ebbba9fff27a7
Galina Decheva <de_galina@abv.bg>
2017-09-10 23:46:59 +0300

    AppendLists

---
+++ AppendLists/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace AppendLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine().Split('|').Reverse().ToList();
+            List<int> result = new List<int>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                List<int> temp = list[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+                result.AddRange(temp);
+            }
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
