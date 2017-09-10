ab9554e0c69fc1c0f92af48b28964e3b067367aa
Galina Decheva <de_galina@abv.bg>
2017-09-10 16:00:27 +0300

    RemoveNegativesAndReverse

--- RemoveNegativesAndReverse/Program.cs
+++ RemoveNegativesAndReverse/Program.cs
@@ -11,15 +11,12 @@ namespace RemoveNegativesAndReverse
         static void Main(string[] args)
         {
             List<int> list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-            List<int> list1 = new List<int>();
-            for (int i = 0; i < list.Count; i++)
-            {
-                list.Remove(-list[i]);
-                
 
-            }
-            
+            list.RemoveAll(a => a < 0);
+            list.Reverse();
+            Console.WriteLine(list.Count == 0 ? "empty": string.Join(" ", list));
 
         }
     }
 }
+
