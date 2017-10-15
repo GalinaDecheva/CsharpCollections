865865511060eca94b82a2bed330c8b6f40539a6
Galina Decheva <de_galina@abv.bg>
2017-10-15 22:54:58 +0300

    MatchPhoneNumber - StringBuilder

---
+++ HowToOverwriteToString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        Human h = new Human("Pesho",12);
+        Console.WriteLine(h);
+    }
+
+}
+
+class Human
+{
+    public string Name { get; set;}
+    public int Age { get; set; }    
+    
+    public Human(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public override string ToString()
+    {
+        return this.Name + " " + this.Age;
+    }
+}
+
+
---
+++ MatchPhoneNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Regex pattern = new Regex(@"\+359([ -])2\1\d{3}\1\d{4}\b");
+            String input = Console.ReadLine();
+
+            MatchCollection matchCollection = pattern.Matches(input);
+            StringBuilder builder = new StringBuilder();
+
+            foreach (Match m in matchCollection) {
+                builder.Append(m + ", ");
+            }
+            if (builder.Length >= 2)
+            {
+                builder.Remove(builder.Length - 2, 2);
+            }
+            Console.WriteLine(builder.ToString());
+
+        }
+    }
+}
