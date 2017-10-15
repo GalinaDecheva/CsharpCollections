e742b19f4186f167bfe082e0a44e85805b3d4b90
Galina Decheva <de_galina@abv.bg>
2017-10-15 17:07:14 +0300

    MatchFUllName

---
+++ MatchFullName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex pattern = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+\b");
+            String names = Console.ReadLine();
+            MatchCollection matches = pattern.Matches(names);
+            foreach (Match m in matches)
+            {
+                Console.WriteLine(m.Groups[0] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Regex/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Regex pattern = new Regex("(?:https?|ftp)://([^/]+)(/.*)");
+        String text = @"sfdafsdfa gdhdg
+                            http://alabala.bg/resource
+                            Text messaging, or texting, ";
+        
+
+        //if (pattern.IsMatch(text))
+        //{
+        //    Console.WriteLine("Pattern matches text");
+
+        //}
+        Match match = pattern.Match(text);
+        match = pattern.Match(text);
+        //Console.WriteLine("First match found: {0}", match.Groups[0]);
+        //Console.WriteLine(match.Groups[1]);
+        //Console.WriteLine(match.Groups[2]);
+        MatchCollection matches = pattern.Matches(text);
+
+        //foreach (Match m in matches)
+        //{
+        //    Console.WriteLine("Match found: {0}", m.Groups[0]);
+        //}
+        //return;
+
+        foreach (Group g in match.Groups)
+        {
+            Console.WriteLine("Group: {0}",g);
+        }
+        //return;
+
+        string result = pattern.Replace(text,"****");
+        Console.WriteLine(result);
+    }
+}
+
